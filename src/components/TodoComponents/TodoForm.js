53bb91d9250c729ebbcdd291da1a4d3a26c903b7
nanoblit <nanoblit@users.noreply.github.com>
2019-05-29 23:24:59 +0200

    remove console.log()

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,17 +2,14 @@ import React from 'react';
 
 const TodoForm = ({
   inputValue, onChangeInput, onAddTodo, onClearCompleted,
-}) => {
-  console.log(JSON.stringify(onChangeInput));
-  return (
-    <form onSubmit={onAddTodo}>
-      <input value={inputValue} onChange={onChangeInput} type="text" />
-      <button type="submit">Add Todo</button>
-      <button onClick={onClearCompleted} type="button">
+}) => (
+  <form onSubmit={onAddTodo}>
+    <input value={inputValue} onChange={onChangeInput} type="text" />
+    <button type="submit">Add Todo</button>
+    <button onClick={onClearCompleted} type="button">
         Clear Completed
-      </button>
-    </form>
-  );
-};
+    </button>
+  </form>
+);
 
 export default TodoForm;
