9f93a99aaf18c89653079c5b4b6553c8bd67baab
nanoblit <nanoblit@users.noreply.github.com>
2019-05-29 23:23:55 +0200

    Replace App class component with functional component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      taskList: [],
-      newTodo: '',
-    };
-  }
+const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     const taskListFromStorage = window.localStorage.getItem('taskList');
     if (taskListFromStorage) {
-      this.setState({ taskList: JSON.parse(taskListFromStorage) });
+      setTaskList(JSON.parse(taskListFromStorage));
     }
-  }
+  }, []);
 
-  componentDidUpdate() {
-    const { taskList } = this.state;
+  useEffect(() => {
     window.localStorage.setItem('taskList', JSON.stringify(taskList));
-  }
+  }, [taskList]);
 
-  crossOut = data => {
-    const { taskList } = this.state;
+  const crossOut = data => {
     const newData = data;
     newData.completed = !newData.completed;
 
-    this.setState({
-      taskList: taskList.map(task => {
+    setTaskList(
+      taskList.map(task => {
         if (task.id === newData.id) {
           return newData;
         }
         return task;
       }),
-    });
+    );
   };
 
-  onChangeInput = e => {
-    this.setState({
-      newTodo: e.target.value,
-    });
+  const onChangeInput = e => {
+    setNewTodo(e.target.value);
   };
 
-  onAddTodo = e => {
+  const onAddTodo = e => {
     e.preventDefault();
-    const { newTodo } = this.state;
 
     if (newTodo === '') return;
 
@@ -56,33 +46,108 @@ class App extends React.Component {
       completed: false,
     };
 
-    this.setState(prevState => ({
-      taskList: [...prevState.taskList, todo],
-      newTodo: '',
-    }));
+    setTaskList([...taskList, todo]);
+    setNewTodo('');
   };
 
-  onClearCompleted = () => {
-    this.setState(prevState => ({
-      taskList: prevState.taskList.filter(({ completed }) => !completed),
-    }));
+  const onClearCompleted = () => {
+    setTaskList(taskList.filter(({ completed }) => !completed));
   };
 
-  render() {
-    const { taskList, newTodo } = this.state;
-
-    return (
-      <div>
-        <TodoList taskList={taskList} crossOut={this.crossOut} />
-        <TodoForm
-          inputValue={newTodo}
-          onChangeInput={this.onChangeInput}
-          onAddTodo={this.onAddTodo}
-          onClearCompleted={this.onClearCompleted}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <TodoList taskList={taskList} crossOut={crossOut} />
+      <TodoForm
+        inputValue={newTodo}
+        onChangeInput={onChangeInput}
+        onAddTodo={onAddTodo}
+        onClearCompleted={onClearCompleted}
+      />
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       taskList: [],
+//       newTodo: '',
+//     };
+//   }
+
+//   componentDidMount() {
+//     const taskListFromStorage = window.localStorage.getItem('taskList');
+//     if (taskListFromStorage) {
+//       this.setState({ taskList: JSON.parse(taskListFromStorage) });
+//     }
+//   }
+
+//   componentDidUpdate() {
+//     const { taskList } = this.state;
+//     window.localStorage.setItem('taskList', JSON.stringify(taskList));
+//   }
+
+//   crossOut = data => {
+//     const { taskList } = this.state;
+//     const newData = data;
+//     newData.completed = !newData.completed;
+
+//     this.setState({
+//       taskList: taskList.map(task => {
+//         if (task.id === newData.id) {
+//           return newData;
+//         }
+//         return task;
+//       }),
+//     });
+//   };
+
+//   onChangeInput = e => {
+//     this.setState({
+//       newTodo: e.target.value,
+//     });
+//   };
+
+//   onAddTodo = e => {
+//     e.preventDefault();
+//     const { newTodo } = this.state;
+
+//     if (newTodo === '') return;
+
+//     const todo = {
+//       task: newTodo,
+//       id: Date.now(),
+//       completed: false,
+//     };
+
+//     this.setState(prevState => ({
+//       taskList: [...prevState.taskList, todo],
+//       newTodo: '',
+//     }));
+//   };
+
+//   onClearCompleted = () => {
+//     this.setState(prevState => ({
+//       taskList: prevState.taskList.filter(({ completed }) => !completed),
+//     }));
+//   };
+
+//   render() {
+//     const { taskList, newTodo } = this.state;
+
+//     return (
+//       <div>
+//         <TodoList taskList={taskList} crossOut={this.crossOut} />
+//         <TodoForm
+//           inputValue={newTodo}
+//           onChangeInput={this.onChangeInput}
+//           onAddTodo={this.onAddTodo}
+//           onClearCompleted={this.onClearCompleted}
+//         />
+//       </div>
+//     );
+//   }
+// }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const TodoForm = (inputValue, onChangeInput, onAddTodo, onClearCompleted) => (
-  <form onSubmit={onAddTodo}>
-    <input value={inputValue} onChange={onChangeInput} type="text" />
-    <button type="submit">Add Todo</button>
-    <button onClick={onClearCompleted} type="button">
+const TodoForm = ({
+  inputValue, onChangeInput, onAddTodo, onClearCompleted,
+}) => {
+  console.log(JSON.stringify(onChangeInput));
+  return (
+    <form onSubmit={onAddTodo}>
+      <input value={inputValue} onChange={onChangeInput} type="text" />
+      <button type="submit">Add Todo</button>
+      <button onClick={onClearCompleted} type="button">
         Clear Completed
-    </button>
-  </form>
-);
+      </button>
+    </form>
+  );
+};
 
 export default TodoForm;
