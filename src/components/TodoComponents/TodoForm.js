08e9c9fcbd74bd85e120466baace1dc3ce26e87f
nanoblit <nanoblit@users.noreply.github.com>
2019-05-30 14:54:41 +0200

    Change Todo to Task on button

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -5,7 +5,7 @@ const TodoForm = ({
 }) => (
   <form onSubmit={onAddTodo}>
     <input value={inputValue} onChange={onChangeInput} type="text" />
-    <button type="submit">Add Todo</button>
+    <button type="submit">Add Task</button>
     <button onClick={onClearCompleted} type="button">
         Clear Completed
     </button>
